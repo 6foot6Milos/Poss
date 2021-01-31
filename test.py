47d7e4501acc5475342e95ae4b99acb1712e55d0
6foot6Milos <75797883+6foot6Milos@users.noreply.github.com>
2021-01-30 21:48:43 -0800

    Added jumping to my character

--- test.py
+++ test.py
@@ -8,28 +8,54 @@ pygame.display.set_caption("John Wick")
 width = 50
 height = 50
 x=100
-y=100
+y=700
 vel=5
+screen_width = 800
+screen_height = 800
+isJump = False
+jumpCount = 10
+
 run = True
 
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(10)
 
+    #Handles quit event
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run=False
 
-
-    keys = pygamme.key.get_pressed()
+    #Fundamental player movement
+    keys = pygame.key.get_pressed()
     
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x > vel:
+        x -= vel
+    if keys[pygame.K_RIGHT] and x < screen_width - width - vel:
+        x += vel
 
-    if keys[pygame.K_RIGHT]:
+    if not (isJump):
+        #if keys[pygame.K_UP] and y > vel:
+            #y -= vel
+        #if keys[pygame.K_DOWN] and y < screen_height - height - vel:
+            #y += vel
+        if keys[pygame.K_SPACE]:
+            isJump = True
 
-    if keys[pygame.K_UP]:
+    #The jumping math using quadratics
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * 0.5 * neg
+            jumpCount-=1 
 
-    if keys[pygame.K_DOWN]:
+        else:
+            isJump = False
+            jumpCount = 10
 
+    #Draws character
+    win.fill((0,0,0))
     pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
     pygame.display.update()
 
